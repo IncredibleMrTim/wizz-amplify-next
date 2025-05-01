8c0852cd50010e078d70ceccc18aa953286d01d6
Tim Smart <tim.smart@timsmarttechnology.co.uk>
2025-05-01 13:58:03 +0100

    updated gitignore to ignore ._*.*

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,8 +3,9 @@ import { defineStorage } from "@aws-amplify/backend";
 export const storage = defineStorage({
   name: "amplifyTeamDrive",
   access: (allow) => ({
-    'product-pictures/{entity_id}/*': [
-      allow.guest.to(['read']),
-      allow.entity('identity').to(['read', 'write', 'delete'])
+    "product-pictures/{entity_id}/*": [
+      allow.guest.to(["read"]),
+      allow.entity("identity").to(["read", "write", "delete"]),
     ],
+  }),
 });
