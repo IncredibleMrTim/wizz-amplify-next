fdaab8ba4edc38df6c1fd74c8e4fc6ff0fad853f
Tim Smart <tim.smart@timsmarttechnology.co.uk>
2025-05-07 11:45:26 +0100

    added update,get and list services for product

--- app/services/products.ts
+++ app/services/products.ts
@@ -19,3 +19,21 @@ export const getFeaturedProducts = async (count?: number) => {
 
   return result.data;
 };
+
+export const updateProduct = async (product: Schema["Product"]["type"]) => {
+  const result = await client.models.Product.update(product);
+
+  return result;
+};
+
+export const getProduct = async (id: string) => {
+  const result = await client.models.Product.get({ id: id });
+
+  return result;
+};
+
+export const getProducts = async (count?: number) => {
+  const result = await client.models.Product.list();
+
+  return result.data;
+};
