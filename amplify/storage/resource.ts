a8c38ea5833cb9bffe25aad6369d958d84cc092c
Tim Smart <tim.smart@timsmarttechnology.co.uk>
2025-05-07 11:37:13 +0100

    updated storage s3 bucket name to match aws

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,7 +1,7 @@
 import { defineStorage } from "@aws-amplify/backend";
 
 export const storage = defineStorage({
-  name: "WIZZINGTON_IMAGE_STORAGE",
+  name: "WIZZINGTON_PRODUCTS",
 
   access: (allow) => ({
     "public/*": [allow.entity("identity").to(["read", "write", "delete"])],
