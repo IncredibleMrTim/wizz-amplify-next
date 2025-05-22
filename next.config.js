4adb4cdcfb3a45ec2a858963a1691d9c063de128
Tim Smart <tim.smart@timsmarttechnology.co.uk>
2025-05-22 15:35:35 +0100

    updated next config with new s3 bucket

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const nextConfig = {
   env: {
     AWS_S3_PRODUCT_IMAGE_PATH: "public/",
     AWS_S3_PRODUCT_IMAGE_URL:
-      "https://amplify-awsamplifygen2-wi-wizzingtonproductimages2-24vlfw9xeqsq.s3.eu-west-2.amazonaws.com/",
+      "https://amplify-awsamplifygen2-wi-wizzingtonproductimages2-8a0hbugzfqjz.s3.eu-west-2.amazonaws.com/",
   },
   images: {
     domains: [
-      "amplify-awsamplifygen2-ti-wizzingtonproductimagesb-6tec0zyalwm7.s3.eu-west-2.amazonaws.com/",
+      "amplify-awsamplifygen2-wi-wizzingtonproductimages2-8a0hbugzfqjz.s3.eu-west-2.amazonaws.com/",
     ],
   },
 };
