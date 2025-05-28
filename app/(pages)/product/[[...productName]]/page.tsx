c3b96ffd9541c038b8fb1d97f205c163afe42e59
Tim Smart <tim.smart@timsmarttechnology.co.uk>
2025-05-28 12:25:45 +0100

    updated product formto work from an array

--- app/(pages)/product/[[...productName]]/page.tsx
+++ app/(pages)/product/[[...productName]]/page.tsx
@@ -4,30 +4,17 @@ import { useAppSelector, useAppDispatch, STORE_PATHS } from "@/stores/store";
 import { useGetProductQuery } from "@/services/product/useGetProductQuery";
 import { Schema } from "amplify/data/resource";
 import { useParams } from "next/navigation";
-import { Calendar } from "@/components/ui/calendar";
-import { Input } from "@/components/ui/input";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/shad/form";
+import { Form } from "@/components/shad/form";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { TbRulerMeasure } from "react-icons/tb";
 import { FiCalendar } from "react-icons/fi";
 import { PiBasket } from "react-icons/pi";
 import { HiShare } from "react-icons/hi2";
-import { nullable, z } from "zod";
-import { Textarea } from "@/components/shad/textarea";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+import { z } from "zod";
 import Link from "next/link";
+import { FormField as FField } from "./FormField";
+import { FaFacebook } from "react-icons/fa6";
 
 const formSchema = z.object({
   waistSize: z.coerce.number().min(0, { message: "Waist size is required" }),
@@ -46,16 +33,13 @@ const formSchema = z.object({
     .max(4, {
       message: "Maximum quantity is 4",
     }),
-  // .or(z.literal("")),
 
   deliveryDate: z.date(),
 });
 
 const ProductPage = () => {
   const params = useParams();
-  const [product, setProduct] = useState<Schema["Product"]["type"] | null>(
-    null
-  );
+
   const { getProductByName } = useGetProductQuery();
   const dispatch = useAppDispatch();
   const currentProduct = useAppSelector(
@@ -91,6 +75,52 @@ const ProductPage = () => {
   >({
     resolver: zodResolver(formSchema),
   });
+
+  const formFields = [
+    {
+      waistSize: {
+        label: "Waist Size",
+        placeholderText: "Enter your waist size",
+        icon: TbRulerMeasure,
+        variant: "number",
+      },
+    },
+    {
+      chestSize: {
+        label: "Chest Size",
+        placeholderText: "Enter your chest size",
+        icon: TbRulerMeasure,
+        variant: "number",
+      },
+    },
+    {
+      quantity: {
+        label: "Quantity",
+        placeholderText: "Enter quantity",
+        icon: PiBasket,
+        variant: "number",
+      },
+    },
+    {
+      deliveryDate: {
+        label: "Delivery Date",
+        placeholderText: "Select delivery date",
+        icon: FiCalendar,
+        variant: "date",
+      },
+    },
+    {
+      notes: {
+        label: "Additional Information",
+        placeholderText:
+          "Describe any additional information you would like to provide",
+        variant: "textarea",
+        classes: {
+          formItem: "flex flex-col items-start w-full",
+        },
+      },
+    },
+  ];
 
   return (
     <div className="flex flex-col gap-4">
@@ -136,8 +166,8 @@ const ProductPage = () => {
                   rel="noopener noreferrer"
                   className="flex gap-2 opacity-70 hover:opacity-100 transition-opacity w-auto"
                 >
-                  Share on Whatsapp:
-                  <HiShare size={20} />
+                  Share on Facebook:
+                  <FaFacebook size={20} />
                 </Link>
               </div>
               <hr />
@@ -145,173 +175,13 @@ const ProductPage = () => {
           </div>
           <div>
             <Form {...form}>
-              <form className="flex flex-col gap-4 items-start">
-                <div className="flex gap-4 w-full">
-                  <FormField
-                    control={form.control}
-                    name="chestSize"
-                    render={({ field }) => {
-                      return (
-                        <FormItem className="flex flex-col items-start w-1/2">
-                          {/* <FormLabel>Chest Size:</FormLabel> */}
-                          <FormControl className="relative">
-                            <div className="relative w-full">
-                              <TbRulerMeasure
-                                size={20}
-                                className="absolute right-2 top-1/2 transform -translate-y-1/2"
-                              />
-                              <Input
-                                type="number"
-                                placeholder="Chest Size"
-                                {...field}
-                                className="w-full !border-0 shadow-none !border-b-1 rounded-none border-b-gray-300 focus-visible:ring-transparent"
-                              />
-                            </div>
-                          </FormControl>
-                        </FormItem>
-                      );
-                    }}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="waistSize"
-                    render={({ field }) => {
-                      return (
-                        <FormItem className="flex flex-col items-start w-1/2">
-                          <FormControl>
-                            <div className="relative w-full">
-                              <TbRulerMeasure
-                                size={20}
-                                className="absolute right-2 top-1/2 transform -translate-y-1/2"
-                              />
-                              <Input
-                                type="number"
-                                placeholder="Waist Size"
-                                {...field}
-                                className="w-full !border-0 shadow-none !border-b-1 rounded-none border-b-gray-300 focus-visible:ring-transparent"
-                              />
-                            </div>
-                          </FormControl>
-                        </FormItem>
-                      );
-                    }}
-                  />
-                </div>
-                <div className="flex gap-4 w-full">
-                  <FormField
-                    control={form.control}
-                    name="quantity"
-                    render={({ field }) => {
-                      return (
-                        <FormItem className="flex flex-col items-start w-1/2">
-                          <FormControl>
-                            <div className="relative w-full">
-                              <PiBasket
-                                size={20}
-                                className="absolute right-2 top-1/2 transform -translate-y-1/2"
-                              />
-                              <Input
-                                type="number"
-                                placeholder="Quantity"
-                                {...field}
-                                className="w-full !border-0 shadow-none !border-b-1 rounded-none border-b-gray-300 focus-visible:ring-transparent"
-                                onChange={(e) => {
-                                  const value = parseInt(e.target.value, 10);
-                                  if (!isNaN(value)) {
-                                    console.log("Setting quantity:", value);
-                                    form.setValue("quantity", value, {
-                                      shouldValidate: true,
-                                    });
-                                  } else {
-                                    form.setValue("quantity", "", {
-                                      shouldValidate: false,
-                                    });
-                                  }
-                                }}
-                              />
-                            </div>
-                          </FormControl>
-                          <FormMessage className="text-red-500 text-sm mt-1" />
-                        </FormItem>
-                      );
-                    }}
-                  />
-
-                  <FormField
-                    control={form.control}
-                    name="notes"
-                    render={({ field }) => {
-                      return (
-                        <FormItem className="flex flex-col items-start w-1/2">
-                          <FormControl>
-                            <Popover>
-                              <PopoverTrigger asChild>
-                                <div className="relative w-full">
-                                  <FiCalendar
-                                    size={20}
-                                    className="absolute right-2 top-1/2 transform -translate-y-1/2"
-                                  />
-                                  <Input
-                                    type="text"
-                                    placeholder="Suggested Delivery Date"
-                                    value={
-                                      (field?.value &&
-                                        new Date(
-                                          field?.value
-                                        ).toLocaleDateString()) ||
-                                      ""
-                                    }
-                                    readOnly
-                                    className="w-full !border-0 shadow-none !border-b-1 rounded-none border-b-gray-300 focus-visible:ring-transparent"
-                                  />
-                                </div>
-                              </PopoverTrigger>
-                              <PopoverContent className="w-auto p-0 bg-white">
-                                <Calendar
-                                  fromDate={defaultDate()}
-                                  initialFocus={true}
-                                  selected={
-                                    field.value ? new Date() : undefined
-                                  }
-                                  mode="single"
-                                  onSelect={(date, day, mod, e) => {
-                                    if (date) {
-                                      field.onChange(date);
-                                      form.setValue("deliveryDate", date, {
-                                        shouldValidate: true,
-                                      });
-                                    }
-                                  }}
-                                />
-                              </PopoverContent>
-                            </Popover>
-                          </FormControl>
-                        </FormItem>
-                      );
-                    }}
-                  />
-                </div>
-                <div className="flex gap-4 w-full">
-                  <FormField
-                    control={form.control}
-                    name="notes"
-                    render={({ field }) => {
-                      return (
-                        <FormItem className="flex flex-col items-start w-full">
-                          <FormLabel>Additional Information:</FormLabel>
-                          <FormControl>
-                            <Textarea
-                              maxLength={1000}
-                              placeholder="Describe any additional information you would like to provide"
-                              {...field}
-                              className="w-full h-32 border-gray-300 focus-visible:ring-transparent shadow-none"
-                            />
-                          </FormControl>
-                        </FormItem>
-                      );
-                    }}
-                  />
-                </div>
+              <form className="flex flex-wrap flex-row gap-y-4 items-start w-full">
+                {formFields.map((field, index) => {
+                  const [name, props] = Object.entries(field)[0];
+                  return (
+                    <FField key={name} form={form} name={name} {...props} />
+                  );
+                })}
               </form>
             </Form>
           </div>
