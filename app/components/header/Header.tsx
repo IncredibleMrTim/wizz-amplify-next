a693d82b1fe79b131749ee23dabbd584b24dc92e
Tim Smart <tim.smart@timsmarttechnology.co.uk>
2025-06-29 15:15:35 +0100

    updated layout of header background and logo

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -10,22 +10,24 @@ const Header = () => {
   const pathname = usePathname();
   return (
     <>
-      <header className="h-48">
+      <header className="h-48 relative">
+        {/* Background image */}
         <div
-          className="flex h-48 bg-no-repeat bg-cover"
+          className="absolute inset-0 w-full h-48 bg-cover bg-no-repeat opacity-60"
           style={{
-            backgroundImage:
-              "linear-gradient(to right, transparent 30%, #00000075 100%), url('/header-model.jpg')",
+            backgroundImage: "url('/header-model.jpg')",
             backgroundPosition: "30%",
           }}
-        >
-          <div className="w-full flex justify-center md:justify-end p-4">
-            <img
-              src="/wizz-logo-trans-v2-flare-stroke.webp"
-              alt="Wizzington Moos Boutique Logo"
-              onClick={() => router.push("/")}
-            />
-          </div>
+        />
+        {/* Gradient overlay */}
+        <div className="flex absolute inset-0 w-full h-48 md:bg-gradient-to-r from-transparent from-20% to-[#f8f8f8] to-100% pointer-events-none" />
+        {/* Content */}
+        <div className="relative z-10 w-full flex justify-center md:justify-end p-4 h-48">
+          <img
+            src="/wizz-logo-trans-v2-flare-stroke.webp"
+            alt="Wizzington Moos Boutique Logo"
+            onClick={() => router.push("/")}
+          />
         </div>
       </header>
       <div className="hidden w-full border-b border-gray-300 h-10 sho md:flex">
