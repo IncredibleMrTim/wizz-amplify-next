cb46524d82a6d17b9e2e5bc501d06787311a75b5
Tim Smart <tim.smart@timsmarttechnology.co.uk>
2025-07-09 17:13:39 +0100

    added sharp to lamda function package json

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,8 @@
-import { defineBackend } from "@aws-amplify/backend";
+import { defineBackend, defineFunction } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 import { storage } from "./storage/resource";
-
+import { thumbnailGenerator } from "./backend/function/thumbnailGenerator/resource";
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
@@ -10,4 +10,5 @@ defineBackend({
   auth,
   data,
   storage,
+  thumbnailGenerator,
 });
